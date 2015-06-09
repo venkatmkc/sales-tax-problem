f8dfced51879ae202ec9eb900d7727230c36955c
Venkatmkc <venkatmkc@gmail.com>
2015-06-09 16:15:34 +0530

    Added spec, formatted item details should be produced

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,16 @@
 public class Item {
+    private int quantity;
+    private String name;
+    private double price;
+
+    public Item(int quantity, String name, double price) {
+        this.quantity = quantity;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "" + quantity + " " + name + " : " + price;
+    }
 }
---
+++ test/ItemTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ItemTest {
+    @Test
+    public void formattedItemDetailShouldBeProduced() {
+        Item item = new Item(1, "music CD", 1.69);
+
+        String actualFormattedItemDetails = item.toString();
+
+        assertThat(actualFormattedItemDetails, is(equalTo("1 music CD : 1.69")));
+    }
+}
